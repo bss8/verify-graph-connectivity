8efe48b8cd558687b2d559c0747eb88a9fec1ac6
boris <borislav.sabotinov@gmail.com>
2019-11-23 17:56:38 -0600

    Adding Graph interface and BasicGraph implementation class.

---
+++ src/main/java/com/borislavsabotinov/connectedgraphs/graphs/BasicGraph.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2019. Borislav S. Sabotinov
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.borislavsabotinov.connectedgraphs.graphs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BasicGraph implements Graph {
+    private Map<Vertex, List<Vertex>> adjVertices;
+
+    BasicGraph() {
+        this.adjVertices = new HashMap<Vertex, List<Vertex>>();
+    }
+
+    void addVertex(String label) {
+        adjVertices.putIfAbsent(new Vertex(label), new ArrayList<>());
+    }
+
+    void removeVertex(String label) {
+        Vertex v = new Vertex(label);
+        adjVertices.values().stream().forEach(e -> e.remove(v));
+        adjVertices.remove(new Vertex(label));
+    }
+
+    void addEdge(String label1, String label2) {
+        Vertex v1 = new Vertex(label1);
+        Vertex v2 = new Vertex(label2);
+        adjVertices.get(v1).add(v2);
+        adjVertices.get(v2).add(v1);
+    }
+
+    void removeEdge(String label1, String label2) {
+        Vertex v1 = new Vertex(label1);
+        Vertex v2 = new Vertex(label2);
+        List<Vertex> eV1 = adjVertices.get(v1);
+        List<Vertex> eV2 = adjVertices.get(v2);
+        if (eV1 != null)
+            eV1.remove(v2);
+        if (eV2 != null)
+            eV2.remove(v1);
+    }
+
+    List<Vertex> getAdjVertices(String label) {
+        return adjVertices.get(new Vertex(label));
+    }
+
+    String printGraph() {
+        StringBuffer sb = new StringBuffer();
+        for(Vertex v : adjVertices.keySet()) {
+            sb.append(v);
+            sb.append(adjVertices.get(v));
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return this.adjVertices.toString();
+    }
+
+    class Vertex {
+        String label;
+        Vertex(String label) {
+            this.label = label;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + getOuterType().hashCode();
+            result = prime * result + ((label == null) ? 0 : label.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Vertex other = (Vertex) obj;
+            if (!getOuterType().equals(other.getOuterType()))
+                return false;
+            if (label == null) {
+                if (other.label != null)
+                    return false;
+            } else if (!label.equals(other.label))
+                return false;
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return label;
+        }
+
+
+        private BasicGraph getOuterType() {
+            return BasicGraph.this;
+        }
+    }
+
+
+    public static void main(String...args) {
+        BasicGraph graph = new BasicGraph();
+        graph.createGraph(graph);
+        System.out.print(graph.toString());
+    }
+
+    void createGraph(BasicGraph graph) {
+
+        graph.addVertex("Bob");
+        graph.addVertex("Alice");
+        graph.addVertex("Mark");
+        graph.addVertex("Rob");
+        graph.addVertex("Maria");
+        graph.addEdge("Bob", "Alice");
+        graph.addEdge("Bob", "Rob");
+        graph.addEdge("Alice", "Mark");
+        graph.addEdge("Rob", "Mark");
+        graph.addEdge("Alice", "Maria");
+        graph.addEdge("Rob", "Maria");
+    }
+}
